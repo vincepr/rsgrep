ac90fda118055aa6c05d8aee0f10c32a8d14f8d7
vincepr <vincepr@gmx.de>
2023-04-11 13:10:59 +0200

    reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    // reading args
+    let args: Vec<String> = env::args().collect();  //-> ["pathToBinary/rsgrep", "searchstring", "file.txt"]
+    if args.len() < 2 {
+        panic!("rsgrep: Error need 2 args to run rsgrep")
+    }
+    let search = &args[1];
+    let path = &args[2];
+    println!("rsgrep: searching for: {}", search);
+    println!("rsgrep: in file: {}", path);
+
+    //reading a file:
+    let data = fs::read_to_string(path)
+        .expect("rsgrep: Error: cant read file at: {path}");
+    println!("{data}");
 }
