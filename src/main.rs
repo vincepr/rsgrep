60db5b7fcb18246ce01adcc8c54ebdd36144b18f
vincepr <vincepr@gmx.de>
2023-04-11 23:31:39 +0200

    fixed error handeling with eprint() > print()

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use rsgrep::Config;                     // import "OUR"Library:
 fn main() {
     // reading in args:
     let cfg = Config::init().unwrap_or_else(|err|{
-        println!("rsgrep-Error: {err}");
+        eprintln!("rsgrep-Error: {err}");
         process::exit(1);
     });
     if let Err(e) = rsgrep::run(&cfg) { // no need to unwrap here since its only () anyways
-        println!("rsgrep-Error: {e}");
+        eprintln!("rsgrep-Error: {e}");
         process::exit(1);
     }
     process::exit(0);
